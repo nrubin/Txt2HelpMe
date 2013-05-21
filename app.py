a63b1831ef78d2de261c563bca3126969def6484
Noam Rubin <rubin.m.noam@gmail.com>
2013-05-20 22:36:22 -0700

    fixed stuff for heroku

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, request, redirect
 import twilio.twiml
+import os
 
 app=Flask(__name__)
 
@@ -11,5 +12,9 @@ def hello_monkey():
 	resp.sms("Hello, Mobile Monkey")
 	return str(resp)
 
-if __name__ == "__main__":
-	app.run(debug=True)
+if __name__ == '__main__':
+    try:
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port, debug=True)
+    except Exception as e:
+        app.logger.debug("%s"%e)
